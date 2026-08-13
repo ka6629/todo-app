a36d6a17a490b0ce19c501aa8668ed4af8d3873f
ka6629 <s25a1076@bene.fit.ac.jp>
2026-08-13 22:47:55 +0900

    画面ひな形

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ToDoList</title>
+</head>
+<body>
+    <h1>My ToDo List</h1>
+</body>
+</html>
